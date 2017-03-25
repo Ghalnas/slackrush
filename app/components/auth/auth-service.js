7cd625f255640eae6ca8546730f8605446f636b9
Ghalnas <frs.grv@gmail.com>
2017-03-25 11:17:13 +0100

    Update auth-service.js

--- app/components/auth/auth-service.js
+++ app/components/auth/auth-service.js
@@ -1,5 +1,5 @@
 app.service('authService', ['$http', function ($http) {
-    var server = "http://172.24.1.113:3000/api/auth";
+    var server = "http://127.0.0.1:3000/api/auth";
     return {
         register: function(user) {
             return $http.post(server+'/register', user);
